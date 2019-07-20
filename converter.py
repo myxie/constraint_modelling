13f175b007d1563443e40470219ed5013fd8d465
myxie <21298244@student.uwa.edu.au>
2019-07-20 20:28:00 +0800

    Graph conversion from shadow library .json format to dzn matricies now implemented.

--- converter.py
+++ converter.py
@@ -35,7 +35,21 @@ A ".dzn" file will have the format:
 """
 
 import networkx as nx
+import json
 import numpy as np
+
+
+def convert_json_to_dzn(jfile, dzn):
+	nxgraph, machines = convert_json_to_nx(jfile)
+
+	comp_table = format_compcost(nxgraph, machines)
+	print(comp_table)
+	comm_table= format_commcost(nxgraph)
+	# comm_table = format_commcost(nxgraph)
+	#
+	# with open(dzn, 'w') as outfile:
+	# 	outfile.write(str(comm_table))
+	# 	outfile.write(str(comp_table))
 
 
 def convert_from_nxobject(nxgraph):
@@ -44,21 +58,67 @@ def convert_from_nxobject(nxgraph):
 	:param nxgraph:
 	:return:
 	"""
-	cost_matrix = np.empty(nxgraph.size+2, nxgraph.size+2)
+	cost_matrix = np.empty(nxgraph.size + 2, nxgraph.size + 2)
 	for node in nxgraph.node:
 		cost_matrix[node, node] = node
 
 	pass
 
 
-def convert_from_json(json):
-	pass
+def convert_json_to_nx(jfile):
+	"""
+	Converts from json to Networkx graph object
+	:param jfile:
+	:return:
+	"""
+	with open(jfile) as graph_data:
+		tmp = json.load(graph_data)
+		graph = nx.readwrite.json_graph.node_link_graph(tmp['graph'])
+		machines = tmp['system']['resource']
+
+	return graph, machines
 
 
-def convert_from_dzn(filename):
-	matrix = []
-	with open(filename, 'w') as dzn:
-		for line in dzn:
-			matrix.append(line)
-	pass
+def format_compcost(graph, machines):
+	# nodes = list(graph.nodes)
+	print(graph.nodes)
+	num_machines = len(machines)
+	compstr = '[|{0}'.format('0, '*num_machines)
+	for node in graph:
+		complist = graph.node[node]['comp']
+		# print(complist)
+		tmp = str(complist).strip('[]')
+		compstr = '{0}\n|{1},'.format(compstr, tmp)
 
+	final_comp = '{0}\n{1}0|];'.format(compstr, '0, '*(num_machines-1))
+	return final_comp
+
+
+def format_commcost(graph):
+	edges = list(graph.edges)
+	num_nodes = len(graph)
+	edge_matrix = np.zeros((num_nodes + 2, num_nodes + 2), dtype=int)
+	for edge in graph.edges:
+		t1, t2 = edge[0], edge[1]
+		# We use t1+1 to skip the first row in our matrix,
+		# which represent our 'source' node
+		edge_matrix[t1+1][t2] = graph[t1][t2]['data_size']
+	row_matrix = ''
+	for row in edge_matrix:
+		row_string = (
+			str(row).strip('[]').lstrip(' ').replace('  ', ' ').replace(' ', ', '))
+		row_matrix = '{0}\n|{1},'.format(row_matrix, row_string)
+		# print(row_string)
+
+	row_matrix = '[{0}|\n];'.format(row_matrix[:-1])
+	print(row_matrix)
+	return edges
+
+
+print(convert_json_to_dzn("test/heft_nocalc.json", 'tmp.dzn'))
+# def convert_from_dzn(filename):
+# 	matrix = []
+# 	with open(filename, 'w') as dzn:
+# 		for line in dzn:
+# 			matrix.append(line)
+# 	pass
